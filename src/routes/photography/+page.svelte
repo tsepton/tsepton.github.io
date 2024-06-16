8db4d529732e7c39050bb40c1496ab5ceffde286
Thibaut Septon <tsepton@gmail.com>
2024-06-16 17:03:28 +0200

    Add loading guard

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -2,21 +2,22 @@
 	import { base } from '$app/paths';
 	import ParallaxScroll from '$lib/components/ParallaxScroll.svelte';
 
-	let data: string[] = [];
 
 	const imageModules: Record<string, () => Promise<any>> = import.meta.glob('/static/photo/*');
 
-	Promise.all(
+	const data: Promise<string[]>= Promise.all(
 		Object.keys(imageModules).map((modulePath) =>
 			imageModules[modulePath]().then(({ default: imageUrl }) => imageUrl.replace('/static', ''))
 		)
-	).then((imageUrls) => {
-		data = imageUrls;
-	});
+	)
 </script>
 
 <h1>Photography</h1>
 
-{#if data.length >= 2}
+{#await data}
+	<p>Loading...</p>
+{:then data}
 	<ParallaxScroll images={data} />
-{/if}
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
