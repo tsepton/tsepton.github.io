060344f4460797c4e2a437bdf02dd02bbefdd042
Thibaut Septon <tsepton@gmail.com>
2024-12-19 12:55:36 +0100

    Fix base path

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import ParallaxScroll from '$lib/components/ParallaxScroll.svelte';
 
-	const imageModules: Record<string, { default: string }> = import.meta.glob('/src/lib/photo/*', {
-		eager: true
-	});
-	const data: string[] = Object.entries(imageModules).flatMap((x) => x[0]);
+	const imageModules: Record<string, () => Promise<any>> = import.meta.glob('/src/photo/*');
+	const data: Promise<string[]> = Promise.all(
+		Object.keys(imageModules).map((modulePath) => imageModules[modulePath]().then((x) => x.default))
+	);
 </script>
 
 <h1>Photography</h1>
 
-<ParallaxScroll images={data} />
+{#await data then data}
+	<ParallaxScroll images={data} />
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
